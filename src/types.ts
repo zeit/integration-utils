086f054601e6d81bd8a21c16132b049782af08b2
luc <luc.leray@gmail.com>
2019-07-16 14:08:47 +0200

    run prettier

--- src/types.ts
+++ src/types.ts
@@ -11,14 +11,25 @@ export interface UiHookPayload {
 	clientState: any;
 	installationUrl: string;
 	projectId?: string | null;
-	query: {[key: string]: string | number | string[]};
+	query: { [key: string]: string | number | string[] };
 	slug: string;
 	integrationId: string;
 	configurationId: string;
 	teamId: string | null;
 	token: string;
-	user: { id: string, username: string, email: string, name: string, profiles: any[], },
-	team?: { id: string, slug: string, name: string, description: string, } | null,
+	user: {
+		id: string;
+		username: string;
+		email: string;
+		name: string;
+		profiles: any[];
+	};
+	team?: {
+		id: string;
+		slug: string;
+		name: string;
+		description: string;
+	} | null;
 }
 
 export interface FetchOptions extends RequestInit {
